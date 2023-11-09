e3e1b1cc1fd0f6632a5df3f33e4e08913ffc2c41
Samuele Musiani <samu@teapot.ovh>
2023-11-09 23:35:02 +0100

    fix: INIT_PS_STATE; using library func

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -5,45 +5,42 @@ LIST_HEAD(pcbFree_h);           /* List of free PCBs                     */
 static int next_pid = 1;
 
 // insert the element pointed to by p onto the pcbFree list.
-void freePcb(pcb_t *p) {
-  p->p_list.next = pcbFree_h.next;
-  p->p_list.prev = &pcbFree_h;
-  pcbFree_h.next = &(p->p_list);
-  p->p_list.next->prev = &(p->p_list);
-}
+void freePcb(pcb_t *p) { list_add(&p->p_list, &pcbFree_h); }
 
 // return NULL if the pcbFree list is empty. Otherwise, remove an element from
 // the pcbFree list, provide initial values for ALL of the PCBs fields and then
 // return a pointer to the removed element. PCBs get reused, so it is important
 // that no previous value persist in a PCB when it gets reallocated.
 
+// I don't know if this is the right way. Maybe we can do tath in a struct
 #define INIT_PS_STATE(state)                                                   \
-  {                                                                            \
-    state->entry_ji = 0;                                                       \
-    state->cause = 0;                                                          \
-    state->status = 0;                                                         \
-    state->pc_epc = 0;                                                         \
-    state->mie = 0;                                                            \
+  ({                                                                           \
+    (state)->entry_hi = 0;                                                     \
+    (state)->cause = 0;                                                        \
+    (state)->status = 0;                                                       \
+    (state)->pc_epc = 0;                                                       \
+    (state)->mie = 0;                                                          \
     for (int _i = 0; _i < STATE_GPR_LEN; _i++) {                               \
-      state->gpr[_i] = 0;                                                      \
+      (state)->gpr[_i] = 0;                                                    \
     }                                                                          \
-  }
+  })
 
 pcb_t *allocPcb() {
-  if (pcbFree_h.next == &pcbFree_h /* && pcbFree_h.prev == &pcbFree_h */)
+  if (list_empty(&pcbFree_h))
     return NULL;
 
+  // For my lsp the following line is a working.
+  // Cast to smaller integer type 'size_tt' (aka 'unsigned int') from 'struct
+  // list_head *'
   pcb_t *p = container_of(pcbFree_h.next, pcb_t, p_list);
-  pcbFree_h.next = p->p_list.next;
-  pcbFree_h.next->prev = &pcbFree_h;
+  list_del(&p->p_list);
 
   INIT_LIST_HEAD(&p->p_list);
   p->p_parent = NULL;
   INIT_LIST_HEAD(&p->p_child);
   INIT_LIST_HEAD(&p->p_sib);
 
-  // NON WORKA
-  INIT_PS_STATE(&(p->p_s));
+  INIT_PS_STATE(&p->p_s);
   p->p_time = 0;
 
   INIT_LIST_HEAD(&p->msg_inbox);
