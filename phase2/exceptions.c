0aff5325891bf8504f31a5c12da3798fce94c2cf
Samuele Musiani <samu@teapot.ovh>
2024-01-31 14:04:59 +0100

    add: basic template for phase2

---
+++ phase2/exceptions.c
@@ -0,0 +1,10 @@
+#include <uriscv/liburiscv.h>
+#include "../headers/types.h"
+
+void uTLB_RefillHandler()
+{
+	setENTRYHI(0x80000000);
+	setENTRYLO(0x00000000);
+	TLBWR();
+	LDST((state_t *)0x0FFFF000);
+}
---
+++ phase2/initial.c
@@ -0,0 +1,6 @@
+#include <uriscv/liburiscv.h>
+
+int main(void)
+{
+	return 0;
+}
