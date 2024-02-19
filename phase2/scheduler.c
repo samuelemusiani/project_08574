4b9d0e7ec3b048f66b35d3f78d94ee715b3699b3
Samuele Musiani <samu@teapot.ovh>
2024-02-19 22:42:58 +0100

    timeslice on var instead of hardcode; moved deadlock_detector()

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -2,6 +2,8 @@
 #include "headers/initial.h"
 #include <uriscv/liburiscv.h>
 
+static int deadlock_detector();
+
 void scheduler()
 {
 	if (process_count <= 0) {
@@ -15,17 +17,22 @@ void scheduler()
 			setMIE(MIE_ALL ^ MIE_MTIE_MASK);
 			setSTATUS(MSTATUS_MIE_BIT);
 			WAIT();
-		} else {
-			// Deadlock for μPandOS is defined as when the Process Count > 0
-			// and the Soft-block Count is 0. Take an appropriate deadlock detected
-			// action; invoke the PANIC BIOS service/instruction [Section 7.3.6-pops].
-			//
-			// We can't panic, the first state of the kernel match deadlock.
-			// PANIC(); // DEADLOCK
+		} else if (deadlock_detector()) {
+			PANIC(); // DEADLOCK
 		}
 	}
 
 	current_process = removeProcQ(&ready_queue);
-	setTIMER(5000); // TIMESCALEADDR ??
+	setTIMER(TIMESLICE);
 	LDST(&current_process->p_s);
+}
+
+static int deadlock_detector()
+{
+	// Deadlock for μPandOS is defined as when the Process Count > 0
+	// and the Soft-block Count is 0. Take an appropriate deadlock detected
+	// action; invoke the PANIC BIOS service/instruction [Section 7.3.6-pops].
+	//
+	// We can't panic, the first state of the kernel match deadlock.
+	return 0;
 }
