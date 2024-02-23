338f1586ca3c76dfd22cca251ac11c95a3ecdf1b
Jabbar03 <ayache.omar@gmail.com>
2024-02-23 10:36:03 +0100

    formatting

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -58,22 +58,39 @@ static void syscall_handler()
 				((state_t *)BIOSDATAPAGE)->reg_a0 =
 					DEST_NOT_EXIST;
 			} else {
-				if (searchPcb(&ready_queue,((pcb_t *)dest))) { // cerco nella ready queue il processo che mi interessa
+				if (searchPcb(
+					    &ready_queue,
+					    ((pcb_t *)dest))) { // cerco nella ready queue il processo che mi interessa
 					//devo modificare il registro inbox del pcb a1 (destination) con il payload a2
-					insertMessage(&((pcb_t *)dest)->msg_inbox,(msg_t *)msg);
+					insertMessage(
+						&((pcb_t *)dest)->msg_inbox,
+						(msg_t *)msg);
 
-				} else if (searchPcb(&unanswered_receive,((pcb_t *)dest))) { //cerco nella coda dei messaggi delle receive bloccate
-					pcb_t *msg_list = outProcQ(&unanswered_receive, ((pcb_t *)dest));
-					insertMessage(&msg_list->msg_inbox,(msg_t *)msg);
+				} else if (searchPcb(
+						   &unanswered_receive,
+						   ((pcb_t *)dest))) { //cerco nella coda dei messaggi delle receive bloccate
+					pcb_t *msg_list =
+						outProcQ(&unanswered_receive,
+							 ((pcb_t *)dest));
+					insertMessage(&msg_list->msg_inbox,
+						      (msg_t *)msg);
 
-				} else if (searchPcb(&pcb_blocked_on_clock,((pcb_t *)dest))) { //cerco un elemento nella coda dei pcb fermati per il tempo
-					pcb_t *msg_list = outProcQ(&pcb_blocked_on_clock,((pcb_t *)dest));
-					insertMessage(&msg_list->msg_inbox,(msg_t *)msg);
+				} else if (searchPcb(
+						   &pcb_blocked_on_clock,
+						   ((pcb_t *)dest))) { //cerco un elemento nella coda dei pcb fermati per il tempo
+					pcb_t *msg_list =
+						outProcQ(&pcb_blocked_on_clock,
+							 ((pcb_t *)dest));
+					insertMessage(&msg_list->msg_inbox,
+						      (msg_t *)msg);
 				} else if (deviceListEmpty()) { //cerco un elemento nella coda dei pcb device, aumento il softblockcount se c'è un campo dentro al messaggio dopo che ho castato a struttra payload (da vedere) in cui c'è un codice che mi dice quale probelma c'è stato (es:DOIO)
-					insertMessage(&((pcb_t *)dest)->msg_inbox,(msg_t *)msg);
+					insertMessage(
+						&((pcb_t *)dest)->msg_inbox,
+						(msg_t *)msg);
 
 				} else { //idk when it should occur but i'll figure it out someday
-					((state_t *)BIOSDATAPAGE)->reg_a0 =	MSGNOGOOD;
+					((state_t *)BIOSDATAPAGE)->reg_a0 =
+						MSGNOGOOD;
 				}
 			}
 			//faccio il load state e ritorno al Nucleo spostando lo stack pointer di +4
@@ -85,18 +102,26 @@ static void syscall_handler()
 		case RECEIVEMESSAGE: {
 			unsigned int sender = ((state_t *)BIOSDATAPAGE)->reg_a1;
 			if (emptyProcQ(
-				    &((pcb_t *)sender)->msg_inbox)) { //la lista dei messaggi è vuota blocco il processo mettendolo nella mia coda
+				    &((pcb_t *)sender)
+					     ->msg_inbox)) { //la lista dei messaggi è vuota blocco il processo mettendolo nella mia coda
 				blockSys(sender);
 			} else if (sender) { //caso sender != 0 -> non ANYMESSAGE), devo cercare nella mia inbox se c'è il messaggio del sender nella inbox
-				if (searchMsg(&((pcb_t *)sender)->msg_inbox,
+				if (searchMsg(
+					    &((pcb_t *)sender)->msg_inbox,
 					    ((pcb_t *)sender))) { //se c'è il messaggio 1 else
-					((state_t *)BIOSDATAPAGE)->reg_a2 =	((unsigned int)popMessage(&((pcb_t *)sender)->msg_inbox,((pcb_t *)sender)));
+					((state_t *)BIOSDATAPAGE)->reg_a2 =
+						((unsigned int)popMessage(
+							&((pcb_t *)sender)
+								 ->msg_inbox,
+							((pcb_t *)sender)));
 				} else { //non c'è il messaggio che del sender che sto cercando nella inbox
 					blockSys(sender);
 				}
 			} else { //la lista dei messaggi non è vuota, prendo il primo messaggio della inbox
 				//metto il messaggio nel registro a2 della BIOSDATA page, il msg e casto ad intero per metterlo nella BIOSDATA page
-				((state_t *)BIOSDATAPAGE)->reg_a2 =	((unsigned int)headMessage(&((pcb_t *)sender)->msg_inbox));
+				((state_t *)BIOSDATAPAGE)->reg_a2 =
+					((unsigned int)headMessage(
+						&((pcb_t *)sender)->msg_inbox));
 			}
 			LDST(((state_t *)BIOSDATAPAGE));
 			((state_t *)BIOSDATAPAGE)->reg_sp += 4;
@@ -113,7 +138,8 @@ static void syscall_handler()
 static int deviceListEmpty()
 {
 	for (int i = 0; i < MAXDEVICE; i++) {
-		if (!emptyProcQ(&pcb_blocked_on_device[i])) { //problema della memcpy
+		if (!emptyProcQ(
+			    &pcb_blocked_on_device[i])) { //problema della memcpy
 			return 1;
 		}
 	}
