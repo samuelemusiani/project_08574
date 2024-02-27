5a3d2593dd5733e53d883b57d2aec957bb377d5c
Samuele Musiani <samu@teapot.ovh>
2024-02-27 11:28:53 +0100

    new scheduler implementation

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -4,25 +4,23 @@
 
 void scheduler()
 {
-	if (list_empty(&ready_queue)) {
-		if (process_count <= 0) {
-			PANIC();
-		} else if (process_count == 1 &&
-			   headProcQ(&ready_queue) ==
-				   ssi_pcb) { // Change when make the fake address
-			HALT();
-		} else {
-			if (softblock_count > 0) {
-				setMIE(MIE_ALL ^ MIE_MTIE_MASK);
-				setSTATUS(MSTATUS_MIE_BIT);
-				WAIT();
-			} else { // process_count > 0 && softblock_count <= 0
-				PANIC(); // DEADLOCK
-			}
-		}
-	} else {
-		current_process = removeProcQ(&ready_queue);
-		setTIMER(TIMESLICE);
-		LDST(&current_process->p_s);
+	if (process_count == 1 &&
+	    headProcQ(&ready_queue) ==
+		    ssi_pcb) { // Change when make the fake address
+		HALT();
 	}
+
+	if (list_empty(&ready_queue)) {
+		if (softblock_count > 0) {
+			setMIE(MIE_ALL ^ MIE_MTIE_MASK);
+			setSTATUS(MSTATUS_MIE_BIT);
+			WAIT();
+		} else { // softblock_count <= 0
+			PANIC(); // DEADLOCK
+		}
+	}
+
+	current_process = removeProcQ(&ready_queue);
+	setTIMER(TIMESLICE);
+	LDST(&current_process->p_s);
 }
