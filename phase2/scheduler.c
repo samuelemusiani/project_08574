533a958c4539eb99a68d092f6cd991ea4e97b14b
Samuele Musiani <samu@teapot.ovh>
2024-03-04 13:45:55 +0100

    add: TIMESCALEADDR to PT

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -22,7 +22,7 @@ void scheduler()
 	}
 
 	current_process = removeProcQ(&ready_queue);
-	setTIMER(TIMESLICE);
+	setTIMER(TIMESLICE * (*((cpu_t *)TIMESCALEADDR)));
 	STCK(tod_timer);
 	LDST(&current_process->p_s);
 }
