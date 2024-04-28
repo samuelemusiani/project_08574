e0adcb4e35b480b869dd3dac5c346bf9b9799244
Lorenzo Peronese <lorenzo.peronese@studio.unibo.it>
2024-04-28 16:56:07 +0200

    Fix printer handling

--- phase3/sst.c
+++ phase3/sst.c
@@ -2,7 +2,7 @@
 #include "headers/utils3.h"
 #include <uriscv/liburiscv.h>
 
-#define TERMSTATMASK 0xFF
+#define WRITESTATUSMASK 0xFF
 
 pcb_PTR sst_pcbs[UPROCMAX];
 
@@ -11,45 +11,86 @@ extern pcb_PTR test_pcb; // WE SHOULD NOT DO THIS. I think the cleanest way is
 			 // sender
 extern pcb_PTR ssi_pcb;
 
-typedef unsigned int devregtr;
-
 static void write(sst_print_t *write_payload, unsigned int asid,
 		  unsigned int dev)
 {
 	// dev can only be IL_PRINTER or IL_TERMINAL
-	if (dev != IL_TERMINAL && dev != IL_PRINTER)
+	if (dev == IL_PRINTER) {
+		// print a string of caracters to the printer
+		// with the same number of the sender ASID (-1)
+		int count = 0;
+		devreg_t *base = (devreg_t *)DEV_REG_ADDR(dev, asid - 1);
+		unsigned int *data0 = &(base->dtp.data0);
+		unsigned int *command = &(base->dtp.command);
+		unsigned int status;
+
+		while (write_payload->length > count) {
+			unsigned int value =
+				(unsigned int)*(write_payload->string + count);
+			ssi_do_io_t do_io = {
+				.commandAddr = data0,
+				.commandValue = value,
+			};
+			ssi_payload_t payload = {
+				.service_code = DOIO,
+				.arg = &do_io,
+			};
+
+			// send char to printer
+			SYSCALL(SENDMESSAGE, (unsigned int)ssi_pcb,
+				(unsigned int)(&payload), 0);
+			SYSCALL(RECEIVEMESSAGE, (unsigned int)ssi_pcb,
+				(unsigned int)(&status), 0);
+			// 1 -> device ready, others are all errors
+			if ((status & WRITESTATUSMASK) != 1)
+				PANIC();
+
+			// send command "print"
+			do_io.commandAddr = command;
+			do_io.commandValue = PRINTCHR;
+			SYSCALL(SENDMESSAGE, (unsigned int)ssi_pcb,
+				(unsigned int)(&payload), 0);
+			SYSCALL(RECEIVEMESSAGE, (unsigned int)ssi_pcb,
+				(unsigned int)(&status), 0);
+			if ((status & WRITESTATUSMASK) != 1)
+				PANIC();
+
+			count++;
+		}
+	} else if (dev == IL_TERMINAL) {
+		// print a string of caracters to the terminal
+		// with the same number of the sender ASID (-1)
+
+		devreg_t *base = (devreg_t *)DEV_REG_ADDR(dev, asid - 1);
+		unsigned int *command = &(base->term.transm_command);
+		unsigned int status;
+
+		int count = 0;
+		while (write_payload->length > count) {
+			unsigned int value =
+				PRINTCHR |
+				(((unsigned int)*(write_payload->string + count))
+				 << 8);
+			ssi_do_io_t do_io = {
+				.commandAddr = command,
+				.commandValue = value,
+			};
+			ssi_payload_t payload = {
+				.service_code = DOIO,
+				.arg = &do_io,
+			};
+			SYSCALL(SENDMESSAGE, (unsigned int)ssi_pcb,
+				(unsigned int)(&payload), 0);
+			SYSCALL(RECEIVEMESSAGE, (unsigned int)ssi_pcb,
+				(unsigned int)(&status), 0);
+
+			if ((status & WRITESTATUSMASK) != RECVD)
+				PANIC();
+
+			count++;
+		}
+	} else
 		PANIC();
-	// print a string of caracters to the terminal
-	// with the same number of the sender ASID (-1)
-
-	// do printers and terminals work in the same way?
-	devregtr *base = (devregtr *)DEV_REG_ADDR(dev, asid - 1);
-	devregtr *command = base + 3;
-	devregtr status;
-
-	int count = 0;
-	while (write_payload->length > count) {
-		devregtr value =
-			PRINTCHR |
-			(((devregtr) * (write_payload->string + count)) << 8);
-		ssi_do_io_t do_io = {
-			.commandAddr = command,
-			.commandValue = value,
-		};
-		ssi_payload_t payload = {
-			.service_code = DOIO,
-			.arg = &do_io,
-		};
-		SYSCALL(SENDMESSAGE, (unsigned int)ssi_pcb,
-			(unsigned int)(&payload), 0);
-		SYSCALL(RECEIVEMESSAGE, (unsigned int)ssi_pcb,
-			(unsigned int)(&status), 0);
-
-		if ((status & TERMSTATMASK) != RECVD)
-			PANIC();
-
-		count++;
-	}
 }
 
 void sst()
