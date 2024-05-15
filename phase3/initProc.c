8b2bec6f94d43ed3066e929b9fe56e41ed4d2ad8
Samuele Musiani <samu@teapot.ovh>
2024-05-15 17:02:19 +0200

    alloc stackTLB and stackGEN to ram

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -7,17 +7,30 @@
 
 pcb_PTR mutex_pcb;
 
+memaddr stacks[UPROCMAX * 2];
+
 void test()
 {
 	initSwapStructs();
 	initSupports();
 
+	state_t tmp;
+	STST(&tmp);
+
+	unsigned int stack_ptr = tmp.reg_sp;
+
+	for (int i = 0; i < UPROCMAX * 2; i++) {
+		stack_ptr -= (2 * QPAGE);
+		stacks[i] = stack_ptr;
+	}
+
 	// Init mutex process
-	state_t mutexstate;
-	STST(&mutexstate);
-	mutexstate.reg_sp = mutexstate.reg_sp - QPAGE;
+	state_t mutexstate = tmp;
+	stack_ptr -= QPAGE;
+	mutexstate.reg_sp = stack_ptr;
 	mutexstate.pc_epc = (memaddr)mutex_proc;
-	mutexstate.status = MSTATUS_MPP_M;
+	mutexstate.status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK |
+			    MSTATUS_MIE_MASK;
 	mutexstate.mie = MIE_ALL;
 	mutex_pcb = p_create(&mutexstate, NULL);
 
@@ -29,7 +42,8 @@ void test()
 		state_t tmpstate;
 		STST(&tmpstate);
 		tmpstate.entry_hi = i << ASIDSHIFT;
-		tmpstate.reg_sp = mutexstate.reg_sp - QPAGE * i;
+		stack_ptr -= QPAGE;
+		tmpstate.reg_sp = stack_ptr;
 		tmpstate.pc_epc = (memaddr)sst;
 		tmpstate.status |= MSTATUS_MPP_M | MSTATUS_MPIE_MASK |
 				   MSTATUS_MIE_MASK;
@@ -38,16 +52,17 @@ void test()
 		support_t *s = allocSupport();
 
 		s->sup_asid = i;
+
 		// PGFAULTEXCEPT
-		context_t tmp = { .stackPtr =
-					  (unsigned int)&s->sup_stackTLB[499],
-				  .status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK |
-					    MSTATUS_MIE_MASK,
-				  .pc = (memaddr)tlb_handler };
+		context_t tmp = { // clang-format off
+      .stackPtr = stacks[i - 1],
+			.status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK | MSTATUS_MIE_MASK,
+			.pc = (memaddr)tlb_handler 
+    }; // clang-format on 
 		s->sup_exceptContext[PGFAULTEXCEPT] = tmp;
 
 		// GENERALEXCEPT
-		tmp.stackPtr = (unsigned int)&s->sup_stackGen[499];
+		tmp.stackPtr = stacks[UPROCMAX + i - 1];
 		tmp.pc = (memaddr)general_exception_handler;
 		s->sup_exceptContext[GENERALEXCEPT] = tmp;
 
